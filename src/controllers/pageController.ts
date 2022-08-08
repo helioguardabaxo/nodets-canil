ecfd3b041ce03449f8dc107a95104b778afea986
helioguardabaxo <helio.guardabaxo@gmail.com>
2022-08-08 15:12:54 -0300

    Adicionado banner e título dinamicamente. Adicionada função para marcar link atual.

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,43 @@
 import { Request, Response } from 'express'
 
+import { createMenuObject } from '../helpers/createMenuObjects'
+
 export const home = (req: Request, res: Response) => {
-    res.render('pages/pages')
+    res.render('pages/pages', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 
 export const dogs = (req: Request, res: Response) => {
-    res.render('pages/dogs')
+    res.render('pages/dogs', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Todos os cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
 }
 
 export const cats = (req: Request, res: Response) => {
-    res.render('pages/cats')
+    res.render('pages/cats', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Todos os gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 
 export const fishes = (req: Request, res: Response) => {
-    res.render('pages/fishes')
+    res.render('pages/fishes', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Todos os peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
